275ab96720f5705ff0f89b2023e1734891bef76f
Goutam Jha <jhagoutam804@gmail.com>
2025-11-08 01:40:19 +0530

    deleteMIddle

---
+++ deleteMiddle.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+Node* deleteMiddle(Node* head){
+    if(head == NULL || head->next == NULL){
+        delete head;
+        return NULL;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+    Node* prev = NULL;
+
+    while(fast != NULL && fast->next != NULL){
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    prev->next = slow->next;
+    delete slow;
+
+    return head;
+}
+
+// Utility to print list
+void printList(Node* head) {
+    while (head) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = deleteMiddle(head);
+
+    cout << "After Deleting Middle Node: ";
+    printList(head);
+
+    return 0;
+}
